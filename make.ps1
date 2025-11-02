abf1cb927666e13a956a8a8ba586911b144a9d14
brando <kootermans@gmail.com>
2025-11-02 14:28:49 -0800

    Remove problematic Copilot removal registry command
    The Copilot auto-removal via Runonce command was causing 'Invalid syntax' errors in reg.exe due to nested quote escaping complexity. Removed this non-essential feature - users can manually remove Copilot after installation if desired. All other ISO modifications remain intact.

--- make.ps1
+++ make.ps1
@@ -1170,7 +1170,8 @@ function Invoke-ISOModding {
 
             # 3. Skip privacy questions and OOBE screens
             reg add "HKLM\TMP_SOFTWARE\Policies\Microsoft\Windows\OOBE" /v DisablePrivacyExperience /t REG_DWORD /d 1 /f | Out-Null
-            reg add "HKLM\TMP_DEFAULT\Software\Microsoft\Windows\CurrentVersion\Runonce" /v "UninstallCopilot" /t REG_SZ /d "powershell.exe -NoProfile -Command ""Get-AppxPackage -Name 'Microsoft.Windows.Ai.Copilot.Provider' | Remove-AppxPackage""" /f | Out-Null
+            # Note: Copilot removal via Runonce has been removed due to command-line escaping complexity
+            # Users can manually remove Copilot post-installation if desired
 
             # 4. Disable telemetry and data collection
             reg add "HKLM\TMP_SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f | Out-Null
